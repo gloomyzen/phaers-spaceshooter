bed753c2917fe4564a95ac71269903de4f613944
Artur Barilo <faustkeller45@gmail.com>
2020-04-11 18:26:20 +0300

    master | update build

--- docker/scripts/prepare_project.sh
+++ docker/scripts/prepare_project.sh
@@ -15,47 +15,63 @@ command=''
 #    EXTRA_EXPORTED_RUNTIME_METHODS="['cwrap', 'ccall']"`
 #command=`emcc ${str} --shell-file ${WORK_DIR}/resources/stub/index.html --emrun -o ${WORK_DIR}${PUBLIC_DIR}/wasm/index.wasm`
 
+function prepare_dir() {
+    #prepare_project clear - for clear all last builds
+#    if [[ -d "${WORK_DIR}${PUBLIC_DIR}" ]]; then
+#        rm -rf ${WORK_DIR}${PUBLIC_DIR}
+#    fi
+    if [[ ! -d "${WORK_DIR}${PUBLIC_DIR}" ]]; then
+        mkdir ${WORK_DIR}${PUBLIC_DIR}
+        chmod -R 777 ${WORK_DIR}${PUBLIC_DIR}
+    fi
+}
+
 function parse_command() {
+#prepare_project --output=canvas --file=canvas.c --file=canvas1.c --arg=WASM=1
     command=''
     for key in "${!args[@]}"; do
 #        if [[ ${key} > 1 ]]; then
-            command="${command} ${args[key]}"
+            echo ${args[key]}
+            IN="--arg=WASM=1"
+            arrIN=(${IN//=/ })
+            echo ${arrIN[0]}
+#            command="${command} ${args[key]}"
 #        fi
     done
-    echo $command
+#    echo $command
 }
-
-case "${args[0]}" in
-    --clear|clear)
-        #prepare_project clear - for clear all last builds
-        if [[ -d "${WORK_DIR}${PUBLIC_DIR}" ]]; then
-            rm -rf ${WORK_DIR}${PUBLIC_DIR}
-        fi
-        if [[ ! -d "${WORK_DIR}${PUBLIC_DIR}" ]]; then
-            mkdir ${WORK_DIR}${PUBLIC_DIR}
-            chmod -R 777 ${WORK_DIR}${PUBLIC_DIR}
-        fi
-        ;;
-    --target|target)
-        parse_command
-        #prepare_project target target_name dependency.cpp dependency2.cpp - for build target
-#        str=""
-#        for key in "${!args[@]}"; do
-#            if [[ ${key} > 1 ]]; then
-#                str="${str} ${WORK_DIR}/${args[key]}"
-#            fi
-#        done
 #
-#        echo $str;
-#        IN="--arg=WASM=1"
-#        arrIN=(${IN//=/ })
-#        echo ${arrIN[0]}
-
-#        command=`emcc ${str} --shell-file ${WORK_DIR}/resources/stub/index.html --emrun -o ${WORK_DIR}${PUBLIC_DIR}/${args[1]}.wasm`
-#        exec ${command}
-        ;;
-    *)
-        echo "Nothing caused.\n"
-        exit 1
-
-esac
+#case "${args[0]}" in
+#    --clear|clear)
+#        #prepare_project clear - for clear all last builds
+#        if [[ -d "${WORK_DIR}${PUBLIC_DIR}" ]]; then
+#            rm -rf ${WORK_DIR}${PUBLIC_DIR}
+#        fi
+#        if [[ ! -d "${WORK_DIR}${PUBLIC_DIR}" ]]; then
+#            mkdir ${WORK_DIR}${PUBLIC_DIR}
+#            chmod -R 777 ${WORK_DIR}${PUBLIC_DIR}
+#        fi
+#        ;;
+#    --target|target)
+#        parse_command
+#        #prepare_project target target_name dependency.cpp dependency2.cpp - for build target
+##        str=""
+##        for key in "${!args[@]}"; do
+##            if [[ ${key} > 1 ]]; then
+##                str="${str} ${WORK_DIR}/${args[key]}"
+##            fi
+##        done
+##
+##        echo $str;
+##        IN="--arg=WASM=1"
+##        arrIN=(${IN//=/ })
+##        echo ${arrIN[0]}
+#
+##        command=`emcc ${str} --shell-file ${WORK_DIR}/resources/stub/index.html --emrun -o ${WORK_DIR}${PUBLIC_DIR}/${args[1]}.wasm`
+##        exec ${command}
+#        ;;
+#    *)
+#        echo "Nothing caused.\n"
+#        exit 1
+#
+#esac
