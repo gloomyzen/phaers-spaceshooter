87953093fd00b45ac3f7726a9d98c5589ef3be76
Artur Barilo <faustkeller45@gmail.com>
2020-07-22 00:52:53 +0300

    master | move old files

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
-#include "Core/GameApplication.h"
+//#include "Core/GameApplication.h"
 #include "Core/moduleDefinitions.h"
 
-USING_NAMESPACE_TGAME_CORE
+//USING_NAMESPACE_TGAME_CORE
 
 int main() {
-  GameApplication app;
-  app.run();
+//  GameApplication app;
+//  app.run();
   return 0;
 }
