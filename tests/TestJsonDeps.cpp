e9130499cfc4e0b70c2ee591f7a1030076a71fcb
Artur Barilo <faustkeller45@gmail.com>
2020-08-05 01:35:19 +0300

    ecs-feature | json read tests

--- tests/TestJsonDeps.cpp
+++ tests/TestJsonDeps.cpp
@@ -1,5 +1,11 @@
 #include <gtest/gtest.h>
+#include <fstream>
+#include <cstdlib>
 #include "rapidjson/document.h"
+#include <rapidjson/istreamwrapper.h>
+#include <rapidjson/writer.h>
+#include <rapidjson/stringbuffer.h>
+#include <rapidjson/ostreamwrapper.h>
 
 int main(int argc, char** argv) {
     testing::InitGoogleTest(&argc, argv);
@@ -18,4 +24,28 @@ TEST(TestJsonDeps, IncludeJsonTest) {
     ASSERT_TRUE(document.HasMember("f"));
     ASSERT_TRUE(document["test"].IsString());
     EXPECT_EQ(document["test"], "row");
+}
+
+TEST(TestJsonFileRead, IncludeJsonTest) {
+  /*** Note: need use ../ because all tests located in test folder */
+  //usefull links: https://rapidjson.org/md_doc_stream.html#FileReadStream
+  //usefull links: https://stackoverflow.com/questions/45248182/how-to-read-json-file-using-rapidjson-and-output-to-stdstring
+  std::ifstream ifs ("../resources/data/nodes/mainMenu.json", std::ifstream::in);
+  EXPECT_TRUE(ifs.is_open());
+
+  //read
+  rapidjson::IStreamWrapper isw { ifs };
+
+  rapidjson::Document document {};
+  document.ParseStream( isw );
+  ASSERT_TRUE(document.IsObject());
+  ASSERT_TRUE(document.HasMember("type"));
+  ASSERT_TRUE(document["type"].IsString());
+  EXPECT_EQ(document["type"], "node");
+
+  //write
+//  rapidjson::StringBuffer buffer {};
+//  rapidjson::Writer<rapidjson::StringBuffer> writer { buffer };
+//  document.Accept( writer );
+//  EXPECT_FALSE(document.HasParseError());
 }
