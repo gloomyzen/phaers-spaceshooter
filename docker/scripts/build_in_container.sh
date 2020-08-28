533b5f93fc94125ee08643c05e4d8e483086954d
Artur Barilo <faustkeller45@gmail.com>
2020-08-28 19:46:12 +0300

    imgui-support | refactor build system, main library, in progress

--- docker/scripts/build_in_container.sh
+++ docker/scripts/build_in_container.sh
@@ -6,5 +6,4 @@ PROJECT_PATH=/var/www
 BUILD_FOLDER=public
 
 # cd ${PROJECT_PATH} && rm -rf ${BUILD_FOLDER} && mkdir ${BUILD_FOLDER} && cd ${BUILD_FOLDER} && source "/var/emsdk/emsdk_env.sh" && CC=emcc CXX=em++ cmake -DBUILD_TYPE=webassembly -DEMSCRIPTEN=1 -DCMAKE_BUILD_TYPE=Debug .. && make -j$(nproc)
-cd ${PROJECT_PATH} && rm -rf ${BUILD_FOLDER} && mkdir ${BUILD_FOLDER} && cd ${BUILD_FOLDER} && source "/var/emsdk/emsdk_env.sh" && CC=emcc CXX=em++ cmake -DEMSCRIPTEN=1 -DCMAKE_BUILD_TYPE=Debug .. && make -j$(nproc)
-cd ${PROJECT_PATH} && rm -rf ${BUILD_FOLDER} && mkdir ${BUILD_FOLDER} && cd ${BUILD_FOLDER} && source "/var/emsdk/emsdk_env.sh" && emcmake cmake -DEMSCRIPTEN=1 -DCMAKE_BUILD_TYPE=Debug .. && make -j$(nproc)
+cd ${PROJECT_PATH} && rm -rf ${BUILD_FOLDER} && mkdir ${BUILD_FOLDER} && cd ${BUILD_FOLDER} && source "/var/emsdk/emsdk_env.sh" && emcmake cmake -DCMAKE_BUILD_TYPE=Debug .. && make -j$(nproc)
